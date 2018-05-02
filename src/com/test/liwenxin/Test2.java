e62b3bb3ef72ab8f77e5a896d4a5ef1cf4071acb
liwenxin <1134199419@qq.com>
2018-05-02 14:05:34 +0800

    新增测试类

---
+++ src/com/test/liwenxin/Test2.java
@@ -0,0 +1,18 @@
+package com.test.liwenxin;
+/**
+ * 测试数组
+ * @author Administrator
+ *
+ */
+public class Test2 {
+	/**
+	 * 程序启动
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String[] atts = {"lwx1","test1","admin1"};
+		for (String str:atts) {
+			System.out.println(str);
+		}
+	}
+}
